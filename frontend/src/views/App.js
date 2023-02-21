8ab46dde8b625de937aec1ee04e36909e6adc154
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2023-02-21 12:50:15 -0800

    frontend: setup structure

---
+++ frontend/src/views/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+        <div className="app-container">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
