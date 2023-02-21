13ecaa36d4d5ea48a529e9038124299721b7b95f
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2023-02-21 13:57:50 -0800

    frontend: create simple nav component

---
+++ frontend/src/components/layouts/navigation/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import styles from "./NavBar.module.scss";
+
+export default function NavBar(props) {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.nav}>
+                <img className={styles.brandlogo} src="/logo-md.png" alt="Logo"></img>
+                <h1 className={styles.brandname}>Vulnalert</h1>
+                <span className={styles.divider}></span>
+                <h2 className={styles.pagename}>{props.pagename}</h2>
+            </nav>
+        </header>
+    );
+}
--- frontend/src/views/App.js
+++ frontend/src/views/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 
+import NavBar from "components/layouts/navigation/NavBar/NavBar"
+
 function App() {
   return (
     <div className="app">
         <div className="app-container">
-
+          <NavBar pagename="Home" />
         </div>
     </div>
   );
