b054f3d9bb6e72a942548abbd5343e7e0b8baeb9
Jollo3333 <essonliu88@gmail.com>
2023-02-22 20:46:27 -0800

    added library card component using bootstrap

--- frontend/src/components/cards/libraryCard.js
+++ frontend/src/components/cards/libraryCard.js
@@ -1,3 +1,44 @@
-import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
-export default LibraryCard;
+
+
+function BasicExample() {
+
+const [checked, setChecked] = useState(false);
+
+  return (
+  
+    <Card style={{ width: '18rem' }}>
+    <Card bg="primary" text="white">
+      <Card.Body>
+        <Card.Title>TressJS</Card.Title>
+        <Card.Text>
+        &#60; 2.13
+        </Card.Text>
+      </Card.Body> 
+      </Card>
+      <Card.Body>
+        <Card.Text>
+          Registered
+        </Card.Text>
+        <Card.Text>
+          August / 12 / 2022
+        </Card.Text>
+          <ToggleButton
+            className="mb-2"
+            id="toggle-check"
+            type="checkbox"
+            variant="outline-primary"
+            checked={checked}
+            value="1"
+            onChange={(e) => setChecked(e.currentTarget.checked)} >
+          Checked
+        </ToggleButton>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default BasicExample;
