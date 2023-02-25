116cb928b5d8f3e09589955b6e2d16cee060aae4
Eric Kim <44147889+taehyunnkim@users.noreply.github.com>
2023-02-25 15:53:55 -0800

    backend: add library prefix endpoint

--- backend/routes/api/v1/controllers/libraries.js
+++ backend/routes/api/v1/controllers/libraries.js
@@ -3,11 +3,53 @@ import fetch from 'node-fetch';
 
 var router = express.Router();
 
+router.get('/:prefix', async function(req, res, next) {
+    if (req.session.isAuthenticated) {
+        const prefix = req.params.prefix;
+        if (prefix) {
+            try {
+                req.models.Library.findOne({ name: { "$regex": "^" + prefix } }, async (err, libraries) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({
+                            error: err.message,
+                            status: "error"
+                        }) 
+                    } else {
+                        res.status(200).json(libraries);
+                    }
+                })
+            } catch (err) {
+                console.log(err);
+                res.status(500).json(
+                    {
+                        status: "error",
+                        error: err
+                    }
+                )
+            }    
+        } else {
+            res.status(400).json(
+                {
+                status: "error",
+                error: "Specify the library prefix"
+                }
+            )
+        }
+    } else {
+        res.status(400).json(
+            {
+            status: "error",
+            error: "User is not logged in"
+            }
+        )
+    }
+});
+
 router.get('/versions/:packageName', async function(req, res, next) {
     if (req.session.isAuthenticated) {
         const packageName = req.params.packageName
         try {
-            
             req.models.Library.findOne({ name: packageName }, async (err, library) => {
                 if (err) {
                     console.log(err);
