6d47eb7154ff23430646b930c20a3c679ecdbafa
Jollo3333 <essonliu88@gmail.com>
2023-02-25 14:51:57 -0800

    checking version api fone

--- backend/routes/api/v1/controllers/libraries.js
+++ backend/routes/api/v1/controllers/libraries.js
@@ -12,40 +12,47 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/versions/:packageName', async function(req, res, next) {
-    const packageName = req.params.packageName
-    if (packageName)  {
-        const response = await fetch(`https://registry.npmjs.org/:${packageName}`)
-        const data = await response.json();
-        if (data && data.versions) {
-            let versionKeys = Object.keys(data.versions);
-        }
+    if (req.session.isAuthenticated) {
+        const packageName = req.params.packageName
+        try {
+            const response = await fetch(`https://registry.npmjs.org/${packageName}`)
+            const data = await response.json()
+            const versionKeys = Object.keys(data.versions)
+            
 
-        console.log("version key :" + versionKeys)
-
-
-        let currPackage = await req.models.Library.find({ library: packageName }, (err, library) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({
-                    "error": err.message,
-                    "status": "error"
-                }) 
-            } else if (!library) {
-                res.status(200).json({versionKeys} );
-            } else {
-                res.status(200).json({
-                    versions: library.versions
-                });
-            }
-        })
-
+            req.models.Library.findOne({ library: packageName }, (err, library) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({
+                        "error": err.message,
+                        "status": "error"
+                    }) 
+                } else if (!library) {
+                    console.log("!library"+versionKeys)
+                    res.status(200).json(versionKeys);
+                } else {
+                    console.log("library+"+library)
+                    res.status(200).json({
+                        versions: library.versions
+                    });
+                }
+            })
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(
+                {
+                    status: "error",
+                    error: err
+                }
+            )
+        }    
     } else {
-        res.status(400).json(
-            {
-                status: "error",
-                error: "User is not logged in"
-             }
-        )
+            res.status(400).json(
+                {
+                    status: "error",
+                    error: "User is not logged in"
+                    }
+                )
     }
 });
 
